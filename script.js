f9cde7bd663ba57113774d92a60511021c5f65fc
cedten62 <148348725+cedten62@users.noreply.github.com>
2025-03-23 15:44:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+function openP4() {
+    document.getElementById("p4").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeP4() {
+    document.getElementById("p4").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openMorpion() {
+    document.getElementById("morpion").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeMorpion() {
+    document.getElementById("morpion").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openSimon() {
+    document.getElementById("simon").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeSimon() {
+    document.getElementById("simon").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openBanque() {
+    document.getElementById("banque").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeBanque() {
+    document.getElementById("banque").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openRpg() {
+    document.getElementById("rpg").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeRpg() {
+    document.getElementById("rpg").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openRpg2() {
+    document.getElementById("rpg2").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeRpg2() {
+    document.getElementById("rpg2").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openMdp() {
+    document.getElementById("mdp").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeMdp() {
+    document.getElementById("mdp").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function open2048() {
+    document.getElementById("2048").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function close2048() {
+    document.getElementById("2048").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openMise() {
+    document.getElementById("mise").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeMise() {
+    document.getElementById("mise").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openRacer() {
+    document.getElementById("racer").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeRacer() {
+    document.getElementById("racer").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openSite() {
+    document.getElementById("site-projets").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeSite() {
+    document.getElementById("site-projets").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openJeu() {
+    document.getElementById("jeu").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeJeu() {
+    document.getElementById("jeu").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
+
+function openLebonangle() {
+    document.getElementById("lebonangle").style.display = "inline-block";
+	document.body.classList.add('modal-open');
+}
+function closeLebonangle() {
+    document.getElementById("lebonangle").style.display = "none";
+	document.body.classList.remove('modal-open');
+}
